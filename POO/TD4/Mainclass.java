5fbfecaaddd926736a5f3544481d3157fbb3e245
johnkyky <ugobattiston@gmail.com>
2020-03-26 16:18:11 +0100

    exo 5.3.3 POO

--- POO/TD4/Mainclass.java
+++ POO/TD4/Mainclass.java
@@ -1,8 +1,23 @@
+import org.apache.commons.math3.complex.*;
+
 public class Mainclass
 {
-	static void main(String[] argv)
-	{
+    public static void main(String[] args)
+    {
+        Complex complex1 = new Complex(3.0, 2.0);
+		Complex complex2 = new Complex(1.0, 4.0);
 
-	}
+		double complex1R = complex1.getReal();
+		double complex1I = complex1.getImaginary();
 
+		Complex multiply2 = complex2.multiply(2);
+
+		boolean equals = complex1.equals(complex2);
+
+
+        System.out.println("complex1 = " + complex1.toString() + " | real_part = " + complex1R + " | imaginary_part = " + complex1I);
+        System.out.println("complex2 = " + complex2.toString() + " | complex2 * 2 = " + multiply2.toString());
+
+        System.out.println("(complex1 = complex2) = " + equals);
+    }
 }
---
+++ POO/TD4/README.md
@@ -0,0 +1,12 @@
+IN404 exercice 5.3.2
+
+Exemple de l'implémentation de la librarie Apache (non présente dans l'archive) dans un projet java.
+La compilation se fait grâce à la commande "make" dans le terminal. La commande "make clear" permet de supprimer les fichiers génénés par la compilation.
+La librairie est supposée se trouver dans un dossier "jars" et est nonmée "commons-math3-3.6.1"
+
+.
+├── jars
+│   └── commons-math3-3.6.1.jar
+├── Mainclass.java
+├── makefile
+└── README.md
